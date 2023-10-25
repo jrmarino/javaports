9aaa4757fd4449e8b5e2197ade293f5287e12315
John Marino <draco@marino.st>
2023-10-25 16:34:58 +0000

    archive compiler-rt16

---
+++ bucket_F8/compiler-rt16/patches/patch-lib_sanitizer__common_sanitizer__platform__limits__freebsd.cpp
@@ -0,0 +1,10 @@
+--- lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp.orig	2023-06-10 22:58:16 UTC
++++ lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp
+@@ -76,7 +76,6 @@
+ #include <sha512.h>
+ #include <stdio.h>
+ #include <stringlist.h>
+-#include <term.h>
+ #include <termios.h>
+ #include <time.h>
+ #include <ttyent.h>
