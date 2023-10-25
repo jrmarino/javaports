dfaafb07cd208008cef389f51e3365bc6f8ffa28
John Marino <draco@marino.st>
2023-10-25 21:58:11 +0000

    add compiler-rt17

---
+++ bucket_70/compiler-rt17/patches/patch-lib_sanitizer__common_sanitizer__platform__limits__freebsd.cpp
@@ -0,0 +1,10 @@
+--- lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp.orig	2023-10-17 06:20:52 UTC
++++ lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp
+@@ -77,7 +77,6 @@
+ #include <sha512.h>
+ #include <stdio.h>
+ #include <stringlist.h>
+-#include <term.h>
+ #include <termios.h>
+ #include <time.h>
+ #include <ttyent.h>
