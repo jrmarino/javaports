1f77549ed3ea702421169d2af0410721c8f4654f
John Marino <draco@marino.st>
2024-04-18 20:38:06 +0000

    add compiler-rt18 port

---
+++ bucket_2A/compiler-rt18/patches/patch-lib_sanitizer__common_sanitizer__platform__limits__freebsd.cpp
@@ -0,0 +1,10 @@
+--- lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp.orig	2024-04-17 00:21:15 UTC
++++ lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp
+@@ -77,7 +77,6 @@
+ #include <sha512.h>
+ #include <stdio.h>
+ #include <stringlist.h>
+-#include <term.h>
+ #include <termios.h>
+ #include <time.h>
+ #include <ttyent.h>
