480e3b0f478f583aef249af85bb80d6a99f954dc
John Marino <draco@marino.st>
2024-10-17 04:54:03 +0000

    add llvm release 19 ports

---
+++ bucket_34/compiler-rt19/patches/patch-lib_sanitizer__common_sanitizer__platform__limits__freebsd.cpp
@@ -0,0 +1,10 @@
+--- lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp.orig	2024-10-15 08:17:37 UTC
++++ lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp
+@@ -77,7 +77,6 @@
+ #include <sha512.h>
+ #include <stdio.h>
+ #include <stringlist.h>
+-#include <term.h>
+ #include <termios.h>
+ #include <time.h>
+ #include <ttyent.h>
---
+++ bucket_6D/llvm19/patches/patch-lib_TargetParser_Triple.cpp
@@ -0,0 +1,10 @@
+--- lib/TargetParser/Triple.cpp.orig	2024-10-15 08:17:37 UTC
++++ lib/TargetParser/Triple.cpp
+@@ -645,6 +645,7 @@ static Triple::OSType parseOS(StringRef
+     .StartsWith("darwin", Triple::Darwin)
+     .StartsWith("dragonfly", Triple::DragonFly)
+     .StartsWith("freebsd", Triple::FreeBSD)
++    .StartsWith("midnightbsd", Triple::FreeBSD)
+     .StartsWith("fuchsia", Triple::Fuchsia)
+     .StartsWith("ios", Triple::IOS)
+     .StartsWith("kfreebsd", Triple::KFreeBSD)
