a6e5e0ed009ce9ba860e2b1210cadd180ca3bee9
John Marino <draco@marino.st>
2025-04-27 01:53:49 +0000

    add compiler-rt20 port

---
+++ bucket_1B/compiler-rt20/patches/patch-lib_sanitizer__common_sanitizer__platform__limits__freebsd.cpp
@@ -0,0 +1,10 @@
+--- lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp.orig	2025-04-16 00:23:49 UTC
++++ lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp
+@@ -77,7 +77,6 @@
+ #include <sha512.h>
+ #include <stdio.h>
+ #include <stringlist.h>
+-#include <term.h>
+ #include <termios.h>
+ #include <time.h>
+ #include <ttyent.h>
